acca2e97dd47c4af8211e9ef11ff69fb2ac07a79
itai2700 <39342966+itai2700@users.noreply.github.com>
2020-05-11 09:40:46 +0300

    created the react app and added materialized css
    created the react app and added materialized css

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play call of duty'}
+    ]
+  }
+  
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    )
+  }
+}
